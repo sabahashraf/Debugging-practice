b4c541fb4f34e5e00bf109d16dd6556862dbf6da
Sabah Sabrina Ashraf <sabahsabrina@yahoo.com>
2022-03-10 11:15:55 -0800

    comment fixed

--- js/app.js
+++ js/app.js
@@ -51,7 +51,7 @@ const switchTab = (id) => {
 };
 
 const createPost = (post) => {
-  console.log(post.description);
+  console.log(post.comments);
  
     const image = post.image;
     const div = document.createElement( "article" );
@@ -122,10 +122,10 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post.comments[0].user}
                       </a>
-                      ${post.comments?.text}
-                    </small>
+                      ${post.comments[0].text}
+                    </small> 
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
                 </div>
